8705eb3b09c94be78e76d1d23ae7f7a1f0527a87
Arnulfo Sandoval <arnulfo3ero@gmail.com>
2021-12-08 14:21:22 -0500

    cambie el principio para que no entre solo con ingresar la direccion

--- welcomepaciente.php
+++ welcomepaciente.php
@@ -1,4 +1,13 @@
 <?php session_start()?>
+<?php
+if (!isset($_SESSION['nombre_user']) /* or something like that */)
+{                     
+    session_unset();
+    session_destroy();
+    header("Location: login.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <h1>Holi, <?php echo implode(', ', $_SESSION['nombre_user']); echo ' '; echo implode(', ', $_SESSION['apellido_user']); ?></h1>
 <p><a href="logout.php">Salir</a>.</p>
