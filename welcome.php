0912490fa68d1f0aab1655d067ed3bec3d0afeee
Arnulfo Sandoval <arnulfo3ero@gmail.com>
2021-12-08 14:19:02 -0500

    estructura de control para no poder entrar via escribir la direccion y ya

--- welcome.php
+++ welcome.php
@@ -1,3 +1,13 @@
+<?php
+if (isset($_SESSION['username']) /* or something like that */)
+{                     
+    session_unset();
+    session_destroy();
+    header("Location: login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <p>Holi, doc</p>
 <p><a href="logout.php">Salir</a>.</p>
